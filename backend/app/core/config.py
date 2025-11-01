6e45686ae09d368cbb3f1de1833fd7cee874c279
Justine-ini <ini_justine@outlook.com>
2025-11-01 15:19:41 +0100

    feat: add postgres docker container

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,14 +5,15 @@ class Settings(BaseSettings):
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
     
     model_config = SettingsConfigDict(
-        env_file="../../.envs/.env.local",  # Fixed: env_file as key
+        env_file="../../.envs/.env.local",
         env_ignore_empty=True,
         extra='ignore'
     )
     
-    API_V1_STR: str = "/api/v1"
-    PROJECT_NAME: str = "Finbank_API"
-    PROJECT_DESCRIPTION: str = "Welcome to Financial Tech with FastAPI"
-    SITE_NAME: str = "Finbank"
+    API_V1_STR: str = ""
+    PROJECT_NAME: str = ""
+    PROJECT_DESCRIPTION: str = ""
+    SITE_NAME: str = ""
+    DATABASE_URL: str = ""
 
 settings = Settings()
